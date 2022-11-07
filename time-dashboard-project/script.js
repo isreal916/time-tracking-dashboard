9940dedbd8e05518f9fd24037e2e517c5813a227
isreal916 <111807946+isreal916@users.noreply.github.com>
2022-11-07 03:49:24 -0800

    Add files via upload

---
+++ time-dashboard-project/script.js
@@ -0,0 +1,52 @@
+// alert('hello world')
+$('#daily').click(()=>{
+    $('#daily').css('color','white')
+    $('#work-current').html("5hrs")
+    $('#work-prev').html('7hrs')
+    $('#study-current').html("0hrs")
+    $('#study-prev').html('1hrs')
+    $('#play-current').html('1hrs')
+    $('#play-prev').html('2hrs')
+    $('#social-current').html('1hrs')
+    $('#social-prev').html('3hrs')
+    
+    $('#selfCare-current').html('1hrs')
+    $('#self-prev').html('3hrs')
+    $('#exercise-current').html('1hrs')
+    $('#exercise-prev').html('1hrs')
+
+})
+$('#weekly').click(()=>{
+    
+    $('#work-current').html("32hrs")
+    $('#work-prev').html('36hrs')
+    $('#study-current').html("4hrs")
+    $('#study-prev').html('7hrs')
+    $('#play-current').html('10hrs')
+    $('#play-prev').html('8hrs')
+    $('#social-current').html('5hrs')
+    $('#social-prev').html('10hrs')
+    
+    $('#selfCare-current').html('2hrs')
+    $('#self-prev').html('2hrs')
+    $('#exercise-current').html('4hrs')
+    $('#exercise-prev').html('5hrs')
+
+})
+$('#monthly').click(()=>{
+    
+    $('#work-current').html("103hrs")
+    $('#work-prev').html('128hrs')
+    $('#study-current').html("13hrs")
+    $('#study-prev').html('19hrs')
+    $('#play-current').html('23hrs')
+    $('#play-prev').html('29hrs')
+    $('#social-current').html('21hrs')
+    $('#social-prev').html('23hrs')
+    
+    $('#selfCare-current').html('7hrs')
+    $('#self-prev').html('11hrs')
+    $('#exercise-current').html('11hrs')
+    $('#exercise-prev').html('18hrs')
+
+})
